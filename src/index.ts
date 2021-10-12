1651faf607b24ccbeaea46c1b340282687bdc0f9
maxmeng93 <maxmeng93@gmail.com>
2021-10-12 18:38:45 +0800

    chore: 使用concurrently包来同时运行serve和type-check命令

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import person from './person';
 console.log(person);
 
 let str: string = '';
-str = '2';
+str = "2";
 
 const app = document.getElementById('app');
 (app as HTMLElement).innerText = '这是一个网页';
