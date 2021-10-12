f9447e848a5f8e39a45a0cd5b7a1ae95b96d504a
maxmeng93 <maxmeng93@gmail.com>
2021-10-12 11:27:38 +0800

    update

--- build.js
+++ build.js
@@ -1,24 +1,27 @@
 "use strict";
 
-require("core-js/modules/es.array.includes.js");
-
-var a = [1, 2, 3, 4, 5];
-console.log(a.includes(7));
-"use strict";
-
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 var _objectWithoutProperties2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutProperties"));
 
-var _x$y$a$b = {
+require("core-js/modules/es.array.includes.js");
+
+require("core-js/modules/es.object.to-string.js");
+
+require("core-js/modules/es.promise.js");
+
+var a = [1, 2, 3, 4, 5];
+console.log(a.includes(7));
+new Promise(function () {});
+var obj = {
   x: 1,
   y: 2,
   a: 3,
   b: 4
-},
-    x = _x$y$a$b.x,
-    y = _x$y$a$b.y,
-    z = (0, _objectWithoutProperties2.default)(_x$y$a$b, ["x", "y"]);
+};
+var x = obj.x,
+    y = obj.y,
+    z = (0, _objectWithoutProperties2.default)(obj, ["x", "y"]);
 console.log(x);
 console.log(y);
 console.log(z);
--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const a = [1, 2, 3, 4, 5];
 console.log(a.includes(7));
+
+new Promise(() => { })
+
+const obj = { x: 1, y: 2, a: 3, b: 4 }
+const { x, y, ...z } = obj;
+console.log(x)
+console.log(y)
+console.log(z)
