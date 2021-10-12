4d064f8d60e3a4e4eea959522cf0a1701e3e5b65
maxmeng93 <maxmeng93@gmail.com>
2021-10-12 10:36:54 +0800

    browserslist

--- build.js
+++ build.js
@@ -4,16 +4,14 @@ function _objectWithoutProperties(source, excluded) { if (source == null) return
 
 function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
 
-let _x$y$a$b = {
+var _x$y$a$b = {
   x: 1,
   y: 2,
   a: 3,
   b: 4
 },
-    {
-  x,
-  y
-} = _x$y$a$b,
+    x = _x$y$a$b.x,
+    y = _x$y$a$b.y,
     z = _objectWithoutProperties(_x$y$a$b, ["x", "y"]);
 
 console.log(x);
