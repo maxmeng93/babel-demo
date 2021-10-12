a53665d9e4840b2f3940b7f5566f0939c5b02c99
maxmeng93 <maxmeng93@gmail.com>
2021-10-12 10:41:39 +0800

    @babel/plugin-transform-runtime 解决转换的重复声明

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
 "use strict";
 
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+var _objectWithoutProperties2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutProperties"));
 
 var _x$y$a$b = {
   x: 1,
@@ -12,16 +12,15 @@ var _x$y$a$b = {
 },
     x = _x$y$a$b.x,
     y = _x$y$a$b.y,
-    z = _objectWithoutProperties(_x$y$a$b, ["x", "y"]);
-
+    z = (0, _objectWithoutProperties2.default)(_x$y$a$b, ["x", "y"]);
 console.log(x);
 console.log(y);
 console.log(z);
 "use strict";
 
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+var _objectWithoutProperties2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutProperties"));
 
 var _x$y$a$b = {
   x: 1,
@@ -31,8 +30,7 @@ var _x$y$a$b = {
 },
     x = _x$y$a$b.x,
     y = _x$y$a$b.y,
-    z = _objectWithoutProperties(_x$y$a$b, ["x", "y"]);
-
+    z = (0, _objectWithoutProperties2.default)(_x$y$a$b, ["x", "y"]);
 console.log(x);
 console.log(y);
 console.log(z);
